f7dd2a5dc5668b43be1d471047df9d7d45c09439
Ralph Cox <ralphsmail@btinternet.com>
2023-05-13 15:16:05 +0100

    Basic Structure

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,18 @@
 module.exports = {
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, node: true, },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:react-three/drei',
+    'plugin:react-three/fiber',
+    'plugin:three'
   ],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'react-three/drei', 'react-three/fiber', 'three'],
   rules: {
     'react-refresh/only-export-components': 'warn',
   },
-}
+};
