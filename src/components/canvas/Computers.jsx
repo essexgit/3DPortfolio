fd5cc6b2b2280ff07cea7f21c2f95fa9d0e93f71
Ralph Cox <ralphsmail@btinternet.com>
2023-05-13 16:29:09 +0100

    sizing and spot colour

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -18,11 +18,12 @@ const Computers = ({ isMobile }) => {
         castShadow
         shadow-mapSize={1024}
       />
-      <pointLight intensity={1} />
+      <pointLight intensity={1}
+        color={0x915eff} />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.7 : 0.75}
-        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        scale={isMobile ? 0.7 : 0.95}
+        position={isMobile ? [0, -3, -2.2] : [0, -2.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
